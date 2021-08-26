bef5c1fbe92ba4d3467b434e25cf0d4303c42fb3
Beatriz <biiadiiego@gmail.com>
2021-08-25 22:32:41 -0300

    Adicionando métodos POST, PUT, DELETE, GET - 25-08-21

---
+++ SpringBoot/BlogPessoalBeatriz/BlogPessoalBeatriz/src/test/java/com/blogbeatriz/BlogPessoalBeatriz/BlogPessoalBeatrizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogbeatriz.BlogPessoalBeatriz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoalBeatrizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
